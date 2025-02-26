2557cf6685fa1a32a4bce4c19aef728663e7051b
BrettMayson <brett@mayson.io>
2025-02-26 00:52:51 -0600

    Update CfgVehicles_Units.hpp

--- addons/manava_roa/CfgVehicles_Units.hpp
+++ addons/manava_roa/CfgVehicles_Units.hpp
@@ -4,4 +4,5 @@ class CLASS(Manava_Roa_Member): CLASS(Base_OPFOR) {
     displayName = "Member";
     identityTypes[] = {"LanguageFRE_F","Head_Tanoan"};
     faction = QCLASS(O_Manava_Roa);
+    genericNames = "TanoanMen";
 };
